c5d0e07a4e827aa3fe99772d48756e4f8d059aaa
Eddie Chang <65884274+EddieCMC@users.noreply.github.com>
2021-07-08 08:45:51 +0800

    use state()

--- app/Main.js
+++ app/Main.js
@@ -1,31 +1,63 @@
-import React from "react"
+import React, { useState, useReducer } from "react"
 import ReactDOM from "react-dom"
 import { BrowserRouter, Switch, Route } from "react-router-dom"
+
+import Axios from "axios"
+Axios.defaults.baseURL = "http://localhost:8080"
 
 // My Components
 import Header from "./components/Header"
 import HomeGuest from "./components/HomeGuest"
+import Home from "./components/Home"
 import Footer from "./components/Footer"
 import About from "./components/About"
 import Terms from "./components/Terms"
+import CreatePost from "./components/CreatePost"
+import ViewSinglePost from "./components/ViewSinglePost"
+import FlashMessages from "./components/FlashMessages"
+import ExampleContext from "./ExampleContext"
 
 function Main() {
+  
+  
+  function ourReducer(){
+
+  }
+
+  const [state, dispatch] = useReducer()
+
+  const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem("complexappToken")))
+  const [flashMessages, setFlashMessages] = useState([])
+
+  function addFlashMessage(msg) {
+    setFlashMessages((prev) => prev.concat(msg))
+  }
+
   return (
-    <BrowserRouter>
-      <Header />
-      <Switch>
-        <Route path="/" exact>
-          <HomeGuest />
-        </Route>
-        <Route path="/about-us" exact>
-          <About />
-        </Route>
-        <Route path="/terms" exact>
-          <Terms />
-        </Route>
-      </Switch>
-      <Footer />
-    </BrowserRouter>
+    <ExampleContext.Provider value={{ addFlashMessage, setLoggedIn }}>
+      <BrowserRouter>
+        <FlashMessages message={flashMessages} />
+        <Header loggedIn={loggedIn} />
+        <Switch>
+          <Route path="/" exact>
+            {loggedIn ? <Home /> : <HomeGuest />}
+          </Route>
+          <Route path="/post/:id">
+            <ViewSinglePost />
+          </Route>
+          <Route path="/create-post">
+            <CreatePost />
+          </Route>
+          <Route path="/about-us" exact>
+            <About />
+          </Route>
+          <Route path="/terms" exact>
+            <Terms />
+          </Route>
+        </Switch>
+        <Footer />
+      </BrowserRouter>
+    </ExampleContext.Provider>
   )
 }
 
